57a47ba694d50832e8cdade188dfc3ba29081bbf
lamquy2106 <50693307+lamquy2106@users.noreply.github.com>
2020-06-19 23:07:31 +0700

    cap nhat lan 1

---
+++ Builamquy_ktgiuaki/Builamquy_ktgiuaki/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Builamquy_ktgiuaki
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Builamquy_ktgiuaki/Builamquy_ktgiuaki/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Builamquy_ktgiuaki.MvcApplication" Language="C#" %>
